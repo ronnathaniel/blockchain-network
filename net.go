5a0019f8c33b963551861cff6f483cc4118ac94e
Ron Nathaniel <rnathaniel7@gmail.com>
2020-11-06 01:50:19 -0500

    formatting

--- net.go
+++ net.go
@@ -1,11 +1,9 @@
-
 package main
 
 import (
-    "github.com/ronnathaniel/blockchain-network/net"
+	"github.com/ronnathaniel/blockchain-network/net"
 )
 
-
 func main() {
-    net.RunNet()
+	net.RunNet()
 }
